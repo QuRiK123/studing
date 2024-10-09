ba8f471c7b2c08e93f6850dd258059c3ba6b458b
Dibir <brejnr@bk.ru>
2024-10-09 23:09:43 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let earth
-let userName
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '3')
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let movie = prompt("Один из последних просмотренных фильмов", 'logan');
+let mark = prompt("На сколько оцените его?", '8');
+
+personalMovieDB.movies[movie] = mark
+
+console.log(personalMovieDB)
